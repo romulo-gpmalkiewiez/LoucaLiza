d687c966e1541544270420aca0f19679f18b68f3
Romulo Goelzer Portolann <romulogoelzer@gmail.com>
2021-11-23 23:56:43 -0300

    Tela - Cadastro de Veículo

--- view/CadastroVeiculo.Designer.cs
+++ view/CadastroVeiculo.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace LoucaLiza.view
 {
-    partial class Form1
+    partial class CadastroVeiculo
     {
         /// <summary>
         /// Required designer variable.
@@ -29,12 +29,152 @@ namespace LoucaLiza.view
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxPlaca = new System.Windows.Forms.TextBox();
+            this.textBoxNascimento = new System.Windows.Forms.TextBox();
+            this.textBoxChassi = new System.Windows.Forms.TextBox();
+            this.textBoxModelo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSalvarCliente = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBoxDiaria = new System.Windows.Forms.ComboBox();
+            this.comboBoxCor = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxPlaca
+            // 
+            this.textBoxPlaca.Location = new System.Drawing.Point(267, 170);
+            this.textBoxPlaca.Multiline = true;
+            this.textBoxPlaca.Name = "textBoxPlaca";
+            this.textBoxPlaca.Size = new System.Drawing.Size(139, 32);
+            this.textBoxPlaca.TabIndex = 54;
+            this.textBoxPlaca.Text = "Placa";
+            // 
+            // textBoxNascimento
+            // 
+            this.textBoxNascimento.Location = new System.Drawing.Point(314, 218);
+            this.textBoxNascimento.Multiline = true;
+            this.textBoxNascimento.Name = "textBoxNascimento";
+            this.textBoxNascimento.Size = new System.Drawing.Size(92, 32);
+            this.textBoxNascimento.TabIndex = 53;
+            this.textBoxNascimento.Text = "Km";
+            // 
+            // textBoxChassi
+            // 
+            this.textBoxChassi.Location = new System.Drawing.Point(16, 170);
+            this.textBoxChassi.Multiline = true;
+            this.textBoxChassi.Name = "textBoxChassi";
+            this.textBoxChassi.Size = new System.Drawing.Size(235, 32);
+            this.textBoxChassi.TabIndex = 52;
+            this.textBoxChassi.Text = "Chassi";
+            // 
+            // textBoxModelo
+            // 
+            this.textBoxModelo.Location = new System.Drawing.Point(16, 122);
+            this.textBoxModelo.Multiline = true;
+            this.textBoxModelo.Name = "textBoxModelo";
+            this.textBoxModelo.Size = new System.Drawing.Size(390, 32);
+            this.textBoxModelo.TabIndex = 51;
+            this.textBoxModelo.Text = "Modelo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 24);
+            this.label1.TabIndex = 50;
+            this.label1.Text = "Editar / Novo Veículo";
+            // 
+            // btnSalvarCliente
+            // 
+            this.btnSalvarCliente.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(176)))), ((int)(((byte)(137)))));
+            this.btnSalvarCliente.FlatAppearance.BorderSize = 0;
+            this.btnSalvarCliente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalvarCliente.Location = new System.Drawing.Point(306, 329);
+            this.btnSalvarCliente.Name = "btnSalvarCliente";
+            this.btnSalvarCliente.Size = new System.Drawing.Size(100, 32);
+            this.btnSalvarCliente.TabIndex = 49;
+            this.btnSalvarCliente.Text = "Salvar";
+            this.btnSalvarCliente.UseVisualStyleBackColor = false;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.ItemHeight = 26;
+            this.comboBox2.Location = new System.Drawing.Point(16, 74);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(163, 32);
+            this.comboBox2.Sorted = true;
+            this.comboBox2.TabIndex = 58;
+            this.comboBox2.Text = "Marca";
+            // 
+            // comboBoxDiaria
+            // 
+            this.comboBoxDiaria.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.comboBoxDiaria.FormattingEnabled = true;
+            this.comboBoxDiaria.ItemHeight = 26;
+            this.comboBoxDiaria.Location = new System.Drawing.Point(16, 266);
+            this.comboBoxDiaria.Name = "comboBoxDiaria";
+            this.comboBoxDiaria.Size = new System.Drawing.Size(116, 32);
+            this.comboBoxDiaria.TabIndex = 57;
+            this.comboBoxDiaria.Text = "Diária R$";
+            // 
+            // comboBoxCor
+            // 
+            this.comboBoxCor.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.comboBoxCor.FormattingEnabled = true;
+            this.comboBoxCor.ItemHeight = 26;
+            this.comboBoxCor.Location = new System.Drawing.Point(16, 218);
+            this.comboBoxCor.Name = "comboBoxCor";
+            this.comboBoxCor.Size = new System.Drawing.Size(183, 32);
+            this.comboBoxCor.Sorted = true;
+            this.comboBoxCor.TabIndex = 59;
+            this.comboBoxCor.Text = "Cor";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(215, 218);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(83, 32);
+            this.textBox1.TabIndex = 60;
+            this.textBox1.Text = "Ano";
+            // 
+            // CadastroVeiculo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(421, 379);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBoxCor);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBoxDiaria);
+            this.Controls.Add(this.textBoxPlaca);
+            this.Controls.Add(this.textBoxNascimento);
+            this.Controls.Add(this.textBoxChassi);
+            this.Controls.Add(this.textBoxModelo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSalvarCliente);
+            this.Name = "CadastroVeiculo";
+            this.Text = "LoucaLiza - Cadastro de Veículo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.TextBox textBoxPlaca;
+        private System.Windows.Forms.TextBox textBoxNascimento;
+        private System.Windows.Forms.TextBox textBoxChassi;
+        private System.Windows.Forms.TextBox textBoxModelo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSalvarCliente;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBoxDiaria;
+        private System.Windows.Forms.ComboBox comboBoxCor;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- view/CadastroVeiculo.cs
+++ view/CadastroVeiculo.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace LoucaLiza.view
 {
-    public partial class Form1 : Form
+    public partial class CadastroVeiculo : Form
     {
-        public Form1()
+        public CadastroVeiculo()
         {
             InitializeComponent();
         }
