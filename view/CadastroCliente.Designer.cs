2411ee97672343bdf512592194f9ddadab48c911
Romulo Goelzer Portolann <romulogoelzer@gmail.com>
2021-11-23 23:36:51 -0300

    Tela - Cadastro Cliente

---
+++ view/CadastroCliente.Designer.cs
@@ -0,0 +1,257 @@
+﻿
+namespace LoucaLiza.view
+{
+    partial class CadastroCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSalvarCliente = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxCNH = new System.Windows.Forms.TextBox();
+            this.textBoxCPF = new System.Windows.Forms.TextBox();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.textBoxTelefone = new System.Windows.Forms.TextBox();
+            this.textBoxNascimento = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxLogradouro = new System.Windows.Forms.TextBox();
+            this.textBoxNumero = new System.Windows.Forms.TextBox();
+            this.textBoxComplemento = new System.Windows.Forms.TextBox();
+            this.textBoxBairro = new System.Windows.Forms.TextBox();
+            this.textBoxCidade = new System.Windows.Forms.TextBox();
+            this.textBoxEstado = new System.Windows.Forms.TextBox();
+            this.textBoxCEP = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSalvarCliente
+            // 
+            this.btnSalvarCliente.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(176)))), ((int)(((byte)(137)))));
+            this.btnSalvarCliente.FlatAppearance.BorderSize = 0;
+            this.btnSalvarCliente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalvarCliente.Location = new System.Drawing.Point(531, 438);
+            this.btnSalvarCliente.Name = "btnSalvarCliente";
+            this.btnSalvarCliente.Size = new System.Drawing.Size(100, 32);
+            this.btnSalvarCliente.TabIndex = 25;
+            this.btnSalvarCliente.Text = "Salvar";
+            this.btnSalvarCliente.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 24);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Editar / Novo Cliente";
+            // 
+            // textBoxCNH
+            // 
+            this.textBoxCNH.Location = new System.Drawing.Point(491, 150);
+            this.textBoxCNH.Multiline = true;
+            this.textBoxCNH.Name = "textBoxCNH";
+            this.textBoxCNH.Size = new System.Drawing.Size(140, 32);
+            this.textBoxCNH.TabIndex = 36;
+            this.textBoxCNH.Text = "CNH";
+            // 
+            // textBoxCPF
+            // 
+            this.textBoxCPF.Location = new System.Drawing.Point(491, 102);
+            this.textBoxCPF.Multiline = true;
+            this.textBoxCPF.Name = "textBoxCPF";
+            this.textBoxCPF.Size = new System.Drawing.Size(140, 32);
+            this.textBoxCPF.TabIndex = 35;
+            this.textBoxCPF.Text = "CPF";
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(15, 102);
+            this.textBoxNome.Multiline = true;
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(459, 32);
+            this.textBoxNome.TabIndex = 34;
+            this.textBoxNome.Text = "Nome";
+            // 
+            // textBoxTelefone
+            // 
+            this.textBoxTelefone.Location = new System.Drawing.Point(218, 150);
+            this.textBoxTelefone.Multiline = true;
+            this.textBoxTelefone.Name = "textBoxTelefone";
+            this.textBoxTelefone.Size = new System.Drawing.Size(120, 32);
+            this.textBoxTelefone.TabIndex = 39;
+            this.textBoxTelefone.Text = "Telefone";
+            // 
+            // textBoxNascimento
+            // 
+            this.textBoxNascimento.Location = new System.Drawing.Point(354, 150);
+            this.textBoxNascimento.Multiline = true;
+            this.textBoxNascimento.Name = "textBoxNascimento";
+            this.textBoxNascimento.Size = new System.Drawing.Size(120, 32);
+            this.textBoxNascimento.TabIndex = 38;
+            this.textBoxNascimento.Text = "Nascimento";
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(15, 150);
+            this.textBoxEmail.Multiline = true;
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(187, 32);
+            this.textBoxEmail.TabIndex = 37;
+            this.textBoxEmail.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 213);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 40;
+            this.label2.Text = "Endereço";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 41;
+            this.label3.Text = "Dados Gerais";
+            // 
+            // textBoxLogradouro
+            // 
+            this.textBoxLogradouro.Location = new System.Drawing.Point(15, 246);
+            this.textBoxLogradouro.Multiline = true;
+            this.textBoxLogradouro.Name = "textBoxLogradouro";
+            this.textBoxLogradouro.Size = new System.Drawing.Size(459, 32);
+            this.textBoxLogradouro.TabIndex = 42;
+            this.textBoxLogradouro.Text = "Logradouro";
+            // 
+            // textBoxNumero
+            // 
+            this.textBoxNumero.Location = new System.Drawing.Point(491, 246);
+            this.textBoxNumero.Multiline = true;
+            this.textBoxNumero.Name = "textBoxNumero";
+            this.textBoxNumero.Size = new System.Drawing.Size(140, 32);
+            this.textBoxNumero.TabIndex = 43;
+            this.textBoxNumero.Text = "Número";
+            // 
+            // textBoxComplemento
+            // 
+            this.textBoxComplemento.Location = new System.Drawing.Point(15, 294);
+            this.textBoxComplemento.Multiline = true;
+            this.textBoxComplemento.Name = "textBoxComplemento";
+            this.textBoxComplemento.Size = new System.Drawing.Size(369, 32);
+            this.textBoxComplemento.TabIndex = 44;
+            this.textBoxComplemento.Text = "Complemento";
+            // 
+            // textBoxBairro
+            // 
+            this.textBoxBairro.Location = new System.Drawing.Point(400, 294);
+            this.textBoxBairro.Multiline = true;
+            this.textBoxBairro.Name = "textBoxBairro";
+            this.textBoxBairro.Size = new System.Drawing.Size(231, 32);
+            this.textBoxBairro.TabIndex = 45;
+            this.textBoxBairro.Text = "Bairro";
+            // 
+            // textBoxCidade
+            // 
+            this.textBoxCidade.Location = new System.Drawing.Point(15, 342);
+            this.textBoxCidade.Multiline = true;
+            this.textBoxCidade.Name = "textBoxCidade";
+            this.textBoxCidade.Size = new System.Drawing.Size(285, 32);
+            this.textBoxCidade.TabIndex = 46;
+            this.textBoxCidade.Text = "Cidade";
+            // 
+            // textBoxEstado
+            // 
+            this.textBoxEstado.Location = new System.Drawing.Point(316, 342);
+            this.textBoxEstado.Multiline = true;
+            this.textBoxEstado.Name = "textBoxEstado";
+            this.textBoxEstado.Size = new System.Drawing.Size(68, 32);
+            this.textBoxEstado.TabIndex = 47;
+            this.textBoxEstado.Text = "Estado";
+            // 
+            // textBoxCEP
+            // 
+            this.textBoxCEP.Location = new System.Drawing.Point(400, 342);
+            this.textBoxCEP.Multiline = true;
+            this.textBoxCEP.Name = "textBoxCEP";
+            this.textBoxCEP.Size = new System.Drawing.Size(124, 32);
+            this.textBoxCEP.TabIndex = 48;
+            this.textBoxCEP.Text = "CEP";
+            // 
+            // CadastroCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 491);
+            this.Controls.Add(this.textBoxCEP);
+            this.Controls.Add(this.textBoxEstado);
+            this.Controls.Add(this.textBoxCidade);
+            this.Controls.Add(this.textBoxBairro);
+            this.Controls.Add(this.textBoxComplemento);
+            this.Controls.Add(this.textBoxNumero);
+            this.Controls.Add(this.textBoxLogradouro);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxTelefone);
+            this.Controls.Add(this.textBoxNascimento);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxCNH);
+            this.Controls.Add(this.textBoxCPF);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSalvarCliente);
+            this.Name = "CadastroCliente";
+            this.Text = "LoucaLiza - Novo Cliente";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalvarCliente;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxCNH;
+        private System.Windows.Forms.TextBox textBoxCPF;
+        private System.Windows.Forms.TextBox textBoxNome;
+        private System.Windows.Forms.TextBox textBoxTelefone;
+        private System.Windows.Forms.TextBox textBoxNascimento;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxLogradouro;
+        private System.Windows.Forms.TextBox textBoxNumero;
+        private System.Windows.Forms.TextBox textBoxComplemento;
+        private System.Windows.Forms.TextBox textBoxBairro;
+        private System.Windows.Forms.TextBox textBoxCidade;
+        private System.Windows.Forms.TextBox textBoxEstado;
+        private System.Windows.Forms.TextBox textBoxCEP;
+    }
+}
