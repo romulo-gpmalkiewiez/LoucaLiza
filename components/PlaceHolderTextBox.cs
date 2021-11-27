05624516783675d41927f86fa2d162796e1004b4
Romulo Goelzer Portolann <romulogoelzer@gmail.com>
2021-11-27 15:08:05 -0300

    Custom component - PlaceHolderTextBox

---
+++ components/PlaceHolderTextBox.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LoucaLiza.View.Components
+{
+    public class PlaceHolderTextBox : TextBox
+    {
+
+        bool isPlaceHolder = true;
+        string _placeHolderText;
+        public string PlaceHolderText
+        {
+            get { return _placeHolderText; }
+            set
+            {
+                _placeHolderText = value;
+                setPlaceholder();
+            }
+        }
+
+        public new string Text
+        {
+            get => base.Text;
+            set
+            {
+                base.Text = value;
+                setPlaceholder();
+            }
+        }
+
+        public PlaceHolderTextBox()
+        {
+            GotFocus += removePlaceHolder;
+            LostFocus += setPlaceholder;
+        }
+
+        private void setPlaceholder(object sender, EventArgs e)
+        {
+            setPlaceholder();
+        }
+
+        private void removePlaceHolder(object sender, EventArgs e)
+        {
+            removePlaceHolder();
+        }
+
+        private void setPlaceholder()
+        {
+            if (string.IsNullOrEmpty(base.Text))
+            {
+                base.Text = PlaceHolderText;
+                this.ForeColor = Color.Gray;
+                this.Font = new Font(this.Font, FontStyle.Italic);
+                isPlaceHolder = true;
+            } else
+            {
+                removePlaceHolder();
+            }
+        }
+
+        private void removePlaceHolder()
+        {
+            if (isPlaceHolder)
+            {
+                base.Text = "";
+                this.ForeColor = System.Drawing.SystemColors.WindowText;
+                this.Font = new Font(this.Font, FontStyle.Regular);
+                isPlaceHolder = false;
+            }
+        }
+
+    }
+}
