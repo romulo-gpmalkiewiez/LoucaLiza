1e54eda5a24536ac6df8c367c7236eb23526d411
Romulo Goelzer Portolann <romulogoelzer@gmail.com>
2021-11-29 22:45:30 -0300

    Cadastro Locação

--- view/CadastroLocacao.Designer.cs
+++ view/CadastroLocacao.Designer.cs
@@ -31,16 +31,21 @@ namespace LoucaLiza.view
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CadastroLocacao));
             this.labelTitle = new System.Windows.Forms.Label();
-            this.textBoxNascimento = new System.Windows.Forms.TextBox();
-            this.textBoxNome = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBoxDataLocacao = new LoucaLiza.View.Components.PlaceHolderTextBox();
+            this.textBoxCliente = new LoucaLiza.View.Components.PlaceHolderTextBox();
+            this.textBoxVeiculo = new LoucaLiza.View.Components.PlaceHolderTextBox();
+            this.textBoxDias = new LoucaLiza.View.Components.PlaceHolderTextBox();
+            this.textBoxDevolucao = new LoucaLiza.View.Components.PlaceHolderTextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.btnLocalizaCliente = new System.Windows.Forms.Button();
             this.btnLocalizaVeiculo = new System.Windows.Forms.Button();
             this.btnSalvarLocacao = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // labelTitle
@@ -53,56 +58,70 @@ namespace LoucaLiza.view
             this.labelTitle.TabIndex = 27;
             this.labelTitle.Text = "Nova Locação";
             // 
-            // textBoxNascimento
+            // textBoxDataLocacao
             // 
-            this.textBoxNascimento.Location = new System.Drawing.Point(16, 70);
-            this.textBoxNascimento.Multiline = true;
-            this.textBoxNascimento.Name = "textBoxNascimento";
-            this.textBoxNascimento.Size = new System.Drawing.Size(120, 32);
-            this.textBoxNascimento.TabIndex = 0;
-            this.textBoxNascimento.Text = "Data de Locação";
+            this.textBoxDataLocacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic);
+            this.textBoxDataLocacao.ForeColor = System.Drawing.Color.Gray;
+            this.textBoxDataLocacao.Location = new System.Drawing.Point(16, 73);
+            this.textBoxDataLocacao.Multiline = true;
+            this.textBoxDataLocacao.Name = "textBoxDataLocacao";
+            this.textBoxDataLocacao.PlaceHolderText = "dd/mm/yyyy HH:mm";
+            this.textBoxDataLocacao.Size = new System.Drawing.Size(120, 32);
+            this.textBoxDataLocacao.TabIndex = 0;
             // 
-            // textBoxNome
+            // textBoxCliente
             // 
-            this.textBoxNome.Location = new System.Drawing.Point(16, 123);
-            this.textBoxNome.Multiline = true;
-            this.textBoxNome.Name = "textBoxNome";
-            this.textBoxNome.Size = new System.Drawing.Size(459, 32);
-            this.textBoxNome.TabIndex = 1;
-            this.textBoxNome.Text = "Cliente";
+            this.textBoxCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic);
+            this.textBoxCliente.ForeColor = System.Drawing.Color.Gray;
+            this.textBoxCliente.Location = new System.Drawing.Point(16, 128);
+            this.textBoxCliente.Multiline = true;
+            this.textBoxCliente.Name = "textBoxCliente";
+            this.textBoxCliente.PlaceHolderText = "Cliente";
+            this.textBoxCliente.Size = new System.Drawing.Size(459, 32);
+            this.textBoxCliente.TabIndex = 1;
+            this.textBoxCliente.TabStop = false;
             // 
-            // textBox1
+            // textBoxVeiculo
             // 
-            this.textBox1.Location = new System.Drawing.Point(16, 176);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(459, 32);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.Text = "Veículo";
+            this.textBoxVeiculo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic);
+            this.textBoxVeiculo.ForeColor = System.Drawing.Color.Gray;
+            this.textBoxVeiculo.Location = new System.Drawing.Point(16, 183);
+            this.textBoxVeiculo.Multiline = true;
+            this.textBoxVeiculo.Name = "textBoxVeiculo";
+            this.textBoxVeiculo.PlaceHolderText = "Veículo";
+            this.textBoxVeiculo.Size = new System.Drawing.Size(459, 32);
+            this.textBoxVeiculo.TabIndex = 2;
+            this.textBoxVeiculo.TabStop = false;
             // 
-            // textBox2
+            // textBoxDias
             // 
-            this.textBox2.Location = new System.Drawing.Point(16, 229);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(120, 32);
-            this.textBox2.TabIndex = 3;
-            this.textBox2.Text = "Dias Locação";
+            this.textBoxDias.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic);
+            this.textBoxDias.ForeColor = System.Drawing.Color.Gray;
+            this.textBoxDias.Location = new System.Drawing.Point(16, 240);
+            this.textBoxDias.Multiline = true;
+            this.textBoxDias.Name = "textBoxDias";
+            this.textBoxDias.PlaceHolderText = "Dias Locação";
+            this.textBoxDias.Size = new System.Drawing.Size(120, 32);
+            this.textBoxDias.TabIndex = 3;
             // 
-            // textBox3
+            // textBoxDevolucao
             // 
-            this.textBox3.Location = new System.Drawing.Point(157, 229);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(120, 32);
-            this.textBox3.TabIndex = 4;
-            this.textBox3.Text = "Dias Locação";
+            this.textBoxDevolucao.Enabled = false;
+            this.textBoxDevolucao.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic);
+            this.textBoxDevolucao.ForeColor = System.Drawing.Color.Gray;
+            this.textBoxDevolucao.Location = new System.Drawing.Point(157, 240);
+            this.textBoxDevolucao.Multiline = true;
+            this.textBoxDevolucao.Name = "textBoxDevolucao";
+            this.textBoxDevolucao.PlaceHolderText = "dd/mm/yyyy HH:mm";
+            this.textBoxDevolucao.Size = new System.Drawing.Size(120, 32);
+            this.textBoxDevolucao.TabIndex = 4;
+            this.textBoxDevolucao.TabStop = false;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 286);
+            this.label2.Location = new System.Drawing.Point(12, 287);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(82, 24);
             this.label2.TabIndex = 44;
@@ -112,7 +131,7 @@ namespace LoucaLiza.view
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(100, 289);
+            this.label3.Location = new System.Drawing.Point(98, 290);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(65, 20);
             this.label3.TabIndex = 45;
@@ -124,7 +143,7 @@ namespace LoucaLiza.view
             this.btnLocalizaCliente.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveBorder;
             this.btnLocalizaCliente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLocalizaCliente.Image = ((System.Drawing.Image)(resources.GetObject("btnLocalizaCliente.Image")));
-            this.btnLocalizaCliente.Location = new System.Drawing.Point(472, 123);
+            this.btnLocalizaCliente.Location = new System.Drawing.Point(472, 128);
             this.btnLocalizaCliente.Name = "btnLocalizaCliente";
             this.btnLocalizaCliente.Size = new System.Drawing.Size(32, 32);
             this.btnLocalizaCliente.TabIndex = 1;
@@ -138,7 +157,7 @@ namespace LoucaLiza.view
             this.btnLocalizaVeiculo.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveBorder;
             this.btnLocalizaVeiculo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLocalizaVeiculo.Image = ((System.Drawing.Image)(resources.GetObject("btnLocalizaVeiculo.Image")));
-            this.btnLocalizaVeiculo.Location = new System.Drawing.Point(472, 176);
+            this.btnLocalizaVeiculo.Location = new System.Drawing.Point(472, 183);
             this.btnLocalizaVeiculo.Name = "btnLocalizaVeiculo";
             this.btnLocalizaVeiculo.Size = new System.Drawing.Size(32, 32);
             this.btnLocalizaVeiculo.TabIndex = 2;
@@ -158,21 +177,71 @@ namespace LoucaLiza.view
             this.btnSalvarLocacao.Text = "Alugar Veículo";
             this.btnSalvarLocacao.UseVisualStyleBackColor = false;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(157, 224);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 46;
+            this.label1.Text = "Devolução Estimada";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 224);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 47;
+            this.label4.Text = "Dias Locação";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 57);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.TabIndex = 48;
+            this.label5.Text = "Data Locação";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 112);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 13);
+            this.label6.TabIndex = 49;
+            this.label6.Text = "Cliente";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 167);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 13);
+            this.label7.TabIndex = 50;
+            this.label7.Text = "Veículo";
+            // 
             // CadastroLocacao
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(521, 402);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnSalvarLocacao);
             this.Controls.Add(this.btnLocalizaVeiculo);
             this.Controls.Add(this.btnLocalizaCliente);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.textBoxNome);
-            this.Controls.Add(this.textBoxNascimento);
+            this.Controls.Add(this.textBoxDevolucao);
+            this.Controls.Add(this.textBoxDias);
+            this.Controls.Add(this.textBoxVeiculo);
+            this.Controls.Add(this.textBoxCliente);
+            this.Controls.Add(this.textBoxDataLocacao);
             this.Controls.Add(this.labelTitle);
             this.Name = "CadastroLocacao";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -185,15 +254,20 @@ namespace LoucaLiza.view
         #endregion
 
         private System.Windows.Forms.Label labelTitle;
-        private System.Windows.Forms.TextBox textBoxNascimento;
-        private System.Windows.Forms.TextBox textBoxNome;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private LoucaLiza.View.Components.PlaceHolderTextBox textBoxDataLocacao;
+        private LoucaLiza.View.Components.PlaceHolderTextBox textBoxCliente;
+        private LoucaLiza.View.Components.PlaceHolderTextBox textBoxVeiculo;
+        private LoucaLiza.View.Components.PlaceHolderTextBox textBoxDias;
+        private LoucaLiza.View.Components.PlaceHolderTextBox textBoxDevolucao;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btnLocalizaCliente;
         private System.Windows.Forms.Button btnLocalizaVeiculo;
         private System.Windows.Forms.Button btnSalvarLocacao;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
